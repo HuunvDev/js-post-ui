9eeaa5b435c343681e1ebd6427b52d9adb3201d4
Huunv <huupho1998@gmail.com>
2022-12-05 23:51:23 +0700

    init commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        addEditPost: resolve(__dirname, 'add-edit-post.html'),
+        postDetail: resolve(__dirname, 'post-detail.html')
+      },
+      external : ['vanilla']
+    }
+  }
+})
